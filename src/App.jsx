9bb26f688cfe5ef3e17854cf3ae54ed0199b9d14
KingPrince001 <johnchege2445@gmail.com>
2023-05-18 22:27:35 +0300

    component naming error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Blogs from './Pages/Blogs'
 import Java from './Pages/Java'
 import Javascript from './Pages/Javascript'
 import Python from './Pages/Python'
-import CPlusPlus from './Pages/CPlusplus' 
+import CPlusPlus from './Pages/CPlusPlus' 
 import './App.css'
 
 function App() {
